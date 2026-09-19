cf92aada40f7af955c132b375d37d93459a789e3
Vũ Khoa <trinhvanvukhoa@gmail.com>
2026-09-20 01:07:01 +0700

    Khởi tạo server

---
+++ BE/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+app.use(cors()); // Cho phép FE gọi API
+app.use(express.json()); // Đọc dữ liệu JSON từ FE gửi lên
+const profilePath = path.join(__dirname, 'data', 'profile.json');
+// API 1: Đọc thông tin Profile
+app.get('/api/profile', (req, res) => {
+    try {
+        const rawData = fs.readFileSync(profilePath, 'utf8');
+        const profile = JSON.parse(rawData);
+        res.json(profile);
+    } catch (error) {
+        res.status(500).json({ message: "Lỗi đọc file" });
+    }
+});
+// API 2: Cập nhật Profile
+app.put('/api/profile', (req, res) => {
+    try {
+        const newProfile = req.body;
+        // Ghi đè dữ liệu mới vào file
+        fs.writeFileSync(profilePath, JSON.stringify(newProfile, null, 2), 'utf8');
+        res.json({ success: true, message: "Đã cập nhật Profile" });
+    } catch (error) {
+        res.status(500).json({ message: "Lỗi ghi file" });
+    }
+});
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Backend chạy tại http://localhost:${PORT}`));
